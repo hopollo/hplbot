1ce7b71d12c826db274caef6f726cd728a1cb86f
HoPollo <pedro725@laposte.net>
2018-10-18 23:56:55 +0300

    Quick fixes + retrain after 2 weeks away

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ var timerOptions = {
 }
 
 var client = new tmi.client(options);
-//var timer = new tmi.timer(timerOptions);
 
 client.connect();
 
@@ -58,11 +57,10 @@ function timer() {
 client.on('chat', function (channel, userstate, message, self) {
     var user = userstate['username'];
     var rank = userstate['badges']; //TODO (hopollo): Add user grade detection (sub, mod, follower, owner, etc)
-    var blackListedNames = ['hplbot', 'streamelements', 'hnlbot', 'moobot', 'wizebot'];
+    var blackListedNames = /(^|\W)(hplbot|streamelements|hnlbot|moobot|wizebot)($|\W)/i;
     var debug = true;
 
-    if (self) { return;}
-    for (var i = 0, len = blackListedNames.length; i < len; i++) { if (user.includes(blackListedNames[i])) { return; } }
+    if (self || blackListedNames.test(user)) { return;}
 
     function log(message) {
         if (debug) {
@@ -74,187 +72,169 @@ client.on('chat', function (channel, userstate, message, self) {
         client.action(channel, message);
     }
 
-    function reply(user, message) {
-        client.action(channel, user, message);
+    function reply(message) {
+        client.action(channel, `${user} ${message}`);
     }
 
     function whisper(user, message) {
-        var username = user;
-        client.whisper(username, message);
+        client.whisper(user, message);
     }
 
     function customApi(url){
         //TODO(hopollo) : CREATE THE FUNCTION TO READ FROM LINKS
     }
 
-
-    // ISSUE : commands !something are not detected by the regex code
-
-
     // SONG
-    var songKeywords = ['!song', 'musique'];
-    for (var i = 0, len = songKeywords.length; i < len; i++){
-        var matchSongKeywords = new RegExp("\\b" + songKeywords[i] + "\\b").test(message);
-        if (matchSongKeywords) {
-            log('Matching word/cmd : ' + songKeywords[i]);
+    const songKeywords =  /(^|\W)(!song|musique|song|zik|morceaux)($|\W)/i;
+    if (songKeywords.test(message)) {
+            log(`Matching word/cmd : ${songKeywords}`);
             //TODO(Hopollo): Add function customApi(UrlOfLastFm)
-        }
     }
 
     // SALUTATION
+    const salutationKeywords = /(^|\W)(yo|salut|bonjour)($|\W)/i;
+    if(salutationKeywords.test(message)) {
+        reply(`HeyGuys`);
+    }
 
-	var salutationKeywords = ['bonjour', 'hey', 'salut', 'HeyGuys', 'slt', 'yo','coucou'];
-    for (var i = 0; i < salutationKeywords.length; i++) {
-        var matchSalutationKeywords = new RegExp("\\b" + salutationKeywords[i] + "\\b").test(message);
-        if (matchSalutationKeywords) {
-            log('Matching word : ' + matchSalutationKeywords[i]);
-            reply(user + ' HeyGuys');
-			return;
-		}
-	}
-	
 	// TROLLING
-    var trollingKeywords = ['KappaPride', 'monkaS', 'LuL', 'Jebaited','TriHard','CoolStoryBob','cmonBruh','BibleThump','<3','DansGame'];
-    for (var i = 0, len = trollingKeywords.length; i < len; i++) {
-        var matchTrollingKeywords = new RegExp("\\b" + trollingKeywords[i] + "\\b").test(message);
-        if (matchTrollingKeywords) {
-            log('Matching Emote : ' + matchTrollingKeywords[i]);
-            send(trollingKeywords[i]);
-            return;
-        }
+    const trollingKeywords = /(^|\W)(KappaPride|monkaS|LuL|Jebaited|TriHard|CoolStoryBob|cmonBruh|BibleThump|<3|DansGame)($|\W)/i;
+    if (trollingKeywords.test(message)) {
+        send(trollingKeywords);
     }
 	
 	// CONFIG
-	var configKeywords = ['!config','comme pc', 'ton setup', 'ta config', 'comme setup', 'ton casque', 'comme casque', 'ton clavier', 'ta cg', 'comme cg', 'carte graphique', 'mic', 'ton micro', 'comme micro', 'matos', 'headset', 'ordi', 'équipements', 'équipement', 'écran', 'ecran', 'ecrans', 'écrans'];
-	for(var i = 0, len = configKeywords.length; i < len; i++){
-        var matchConfigKeywords = new RegExp("\\b" + configKeywords[i] + "\\b").test(message); 
-        if (matchConfigKeywords) {
-            log('Matching word/cmd :' + configKeywords[i]);
-			send('Config PC ► goo.gl/LNaxad ou en description, pour les configs jeux utilise !(nomdujeu)config (ex: !rustconfig)');
-			return;
-		}
+	const configKeywords = /(^|\W)(!config|comme pc|ton setup|ta config|comme setup|ton casque|comme casque|ton clavier|ta cg|comme cg|carte graphique|mic|ton micro|comme micro|matos|headset|ordi|équipements|équipement|écran|ecran|ecrans|écrans)($|\W)/i;
+	if(configKeywords.test(message)) {
+        log(`Matching word/cmd :${configKeywords}`);
+        send('Config PC ► goo.gl/LNaxad ou en description, pour les configs jeux utilise !(nomdujeu)config (ex: !rustconfig)');
 	}
 	
 	// VOCAL Discord, Curse
-	var vocalKeywords = ['!discord','vocal', 'discord', 'teamspeak', 'mumble', 'curse', 'skype'];
-    for (var i = 0, len = vocalKeywords.length; i < len; i++) {
-        var matchVocalKeywords = new RegExp("\\b" + vocalKeywords[i] + "\\b").test(message);
-		if (matchVocalKeywords) {
-            send('Serveur Discord ► goo.gl/uRqQn0 (conditions : Mature, Bon micro, 0 bruit de fond)');
-			return;
-		}
-	}
+	const vocalKeywords = /(^|\W|\!)(discord|vocal|discord|teamspeak|mumble|curse|skype)($|\W)/i;
+    if (vocalKeywords.test(message)) {
+        send('Serveur Discord ► goo.gl/uRqQn0 (conditions : Mature, Bon micro, 0 bruit de fond)');
+    }
 	
 	//Background
-	var backgroundCommand = '!background';
+	const backgroundCommand = '!background';
     if (message.includes(backgroundCommand)) {
-        reply(user + ' Mon background ► bit.ly/2a4MRiY');
-		return;
+        reply('Mon background ► bit.ly/2a4MRiY');
     }
 
     //Link detection
-    var linkWhitelist = ["https://clips.twitch.tv/","https://www.youtube.com/watch?v="]
-
+    const linkWhitelist = ["https://clips.twitch.tv/","https://www.youtube.com/watch?v="];
     for (var i = 0, len = linkWhitelist.length; i < len; i++) {
         if (message.includes(linkWhitelist[i])) {
             var source = linkWhitelist.indexOf(linkWhitelist[i]);
-            var user = user;
             linkAnswer(source, user);
         }
-
     }
 
     function linkAnswer(source, user) {
         switch (source) {
             case 0:
-                reply('Merci pour le clip ' + user + ' !');
+                // twitch clip link
+                reply(`Merci pour le clip !`);
                 break;
             case 1:
                 // youtube link
+                reply(`Merci pour la video !`);
+                break;
             default:
                 timeout(user);
         }
     }
 
-    // Timeout and Bans
-    function timeout(user) {
-        console.log(user, ' should be timed out for link violation (' + message + ')');
-        // TODO (hopollo) : ADD mods, owner, sub, follower check to avoid or not timeout
-        client.timeout(channel, user, 30)
-        reply('Timeout : ' + user + '(link violation)');
-    }
-
 	// StreamStatus
-	var streamCommand = ['!setgame','!settitle','!addfilter','!delfilter'];
+	const streamCommand = /(^|\W)(!setgame|!settitle|!addfilter|!delfilter)($|\W)/i;
+    // REMARK (hopollo): Should stay in this order to make sure the command is on a new line
+    if(streamCommand.test(message)){
+        var userRank = userstate['user-type']; // REMARK (hopollo) : return null on broadcaster
+        var message = message.split(' ');
+        var args = [];
+
+        for(var i=1, len=message.length; i < len; i++) {
+            args.push(message[i]);
+        }
+        log(`Edit cmd : ${user} (${userRank}) -> ${message[0]}`);
+        var command = message[0];
+        editChannelInfo(command, args);
+    }
 	
-    for (var i = 0, len = streamCommand.length; i < len; i++) {
-        var matchStreamCommand = new RegExp("\\b" + message + "\\b").test(streamCommand[i]);
-        // REMARK (hopollo): Should stay in this order to make sure the command is on a new line
-		if(matchStreamCommand){
-			var e = streamCommand[i];
-			var userRank = userstate['user-type']; // REMARK (hopollo) : return null on broadcaster
-			
-			log('Edit cmd : ' + user + '(' + userRank + ') -> ' + e);
-            editChannelInfo(command);
-		}
-	}
-	
-	function editChannelInfo(command){
-		switch (command) {
-			case '!setgame':
-							// TODO(hopollo) : ADD editing game to mods and owner
-			case '!settitle':
-							// TODO(hopollo) : ADD editing title to owner only
-			case 'addFilter':
-							// TODO(hopollo) : ADD editing (add) filter to mods and owner
-			case 'delfilter':
-							// TODO(hopollo) : ADD editing (remove) filter to mods and owner
-			
-			default:
-				console.log('default');
-		}
-	}
-	
-	//Social
-    var socialKeywords = ['!social','!facebook','!twitter','!youtube','facebook', 'youtube', 'twitter'];
-    for (var i = 0, len = socialKeywords.length; i < len; i++) {
-        var matchSocialKeywords = new RegExp("\\b" + socialKeywords[i] + "\\b").test(message);
-        if (matchSocialKeywords) {
-            log('Matching word/cmd: ' + matchSocialKeywords[i]);
-            send('Retrouvez moi sur les réseaux ► https://www.twitter.com/HoPolloTV • https://www.youtube.com/HoPollo (!last) • https://www.facebook.com/HoPollo •');
+	function editChannelInfo(command, args) {
+        if (args.length == 0) { reply(`la commande ${command} est incomplète`); }
+        switch (command) {
+            case '!setgame':
+                log(`${user} just !setgame -> ${args}`);
+                break;
+                            // TODO(hopollo) : ADD editing game to mods and owner
+                            // TODO(hopollo) : ADD Twitch game depending on their choises
+            case '!settitle':
+                log(`${user} just !settitle -> ${args}`);
+                break;
+                            // TODO(hopollo) : ADD editing title to owner only
+            case 'addFilter':
+                log(`${user} just !addfilter -> ${args}`);
+                break;
+                            // TODO(hopollo) : ADD editing (add) filter to mods and owner
+            case 'delfilter':
+                log(`${user} just !delfilter -> ${args}`);
+                break;
+                            // TODO(hopollo) : ADD editing (remove) filter to mods and owner
+            
+            default:
+                log('Default EditChannelInfo');
         }
     }
+    
+    //Stats
+    const statsCommands = ['!viewers','!subs','!follows','!views'];
+    //TODO(hopollo) : Implement those API when customAPI is done;
+	
+	//Social
+    const socialKeywords = /(^|\W)(!social|!facebook|!twitter|!youtube|facebook|twitter)($|\W)/i;
+    if (socialKeywords.test(message)) {
+        log(`Matching word/cmd ${socialKeywords}`);
+        send('Retrouvez moi sur les réseaux ► https://www.twitter.com/HoPolloTV • https://www.youtube.com/HoPollo (!last) • https://www.facebook.com/HoPollo •');
+    }
 
-    var lastCommand = '!last';
+    const lastCommand = '!last';
     if (message.includes(lastCommand)) {
-        //TODO (hopollo): return links from lastest youtube video (+ latest twitch vod)
-        return;
+        //TODO (hopollo): return links from lastest youtube video + latest twitch vod
     }
 
-    var hostCommand = '!host';
+    const hostCommand = '!host';
     if (message.includes(hostCommand)) {
-        whisper(user, 'Pour host HoPollo rdv sur : twitch.tv/' + user + ' et écrire (ou copier/coller) dans ton tchat : /host hopollo');
-        return;
+        whisper(user, `Pour host HoPollo rdv sur : twitch.tv/${user} et écrire (ou copier/coller) dans ton tchat : /host hopollo`);
     }
+});
 
-    // Events
+// Timeout and Bans
+function timeout(user) {
+    console.log(`${user} should be timed out for link violation (${message})`);
+    // TODO (hopollo) : ADD mods, owner, sub, follower check to avoid or not timeout
+    client.timeout(channel, user, 30)
+    reply(`Timeout : ${user} (link violation)`);
+}
+
+// Events
     // TODO(hopollo) : MOVE THIS PART TO THE PROPER SECTION (OUT OF MESSAGE POST EVENT)
     // host
     client.on('hosted', function (channel, username, viewers, autohost) {
         var viewersLimit = 4;
         if (viewers < viewersLimit) {
-            send('HOST : ' + username + '(' + viewers + '), bienvenue !');
+            send(`HOST : ${username} (${viewers}), bienvenue !`);
         }
     });
     // cheer
     client.on('cheer', function (channel, userstate, message) {
         var user = userstate['username'];
         var bits = userstate.bits;
-        send('CHEER : ' + user + ' (' + bits + '), merci !');
+        send(`CHEER : ${user} (${bits}), merci !`);
     });
     // subs
     client.on("subscription", function (channel, username, method, message, userstate) {
         // TOOD(hopollo) : work on this when sub button is enabled
-        send('SUB : ' + user + '(' + method + ')');
+        send(`SUB : ${user} (${method})`);
     });
-});
